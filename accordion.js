74f19f1c1a7cd1c971de105f6634a0cb54614ecc
Harshita Raghav <raghav.harshita21@gmail.com>
2023-03-27 00:36:12 +0530

    Updated File

--- accordion.js
+++ accordion.js
@@ -1,7 +1,26 @@
-const accordion = document.getElementsByClassName('accordion-container');
+// const accordion = document.getElementsByClassName('accordion-container');
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+// for (i=0; i<accordion.value.length; i++) {
+//   accordion[i].addEventListener('click', function () {
+//     this.classList.toggle('active')
+//   })
+// }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel /
+    this.classList.toggle("active");
+
+    / Toggle between hiding and showing the active panel */
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
