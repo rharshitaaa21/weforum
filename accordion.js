90320a27df991fcf9b1e28b59a034ba79dcf5b81
Harshita Raghav <raghav.harshita21@gmail.com>
2023-03-27 11:11:51 +0530

    Updated changes

--- accordion.js
+++ accordion.js
@@ -1,20 +1,10 @@
-// const accordion = document.getElementsByClassName('accordion-container');
 
-// for (i=0; i<accordion.value.length; i++) {
-//   accordion[i].addEventListener('click', function () {
-//     this.classList.toggle('active')
-//   })
-// }
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel /
-    this.classList.toggle("active");
 
-    / Toggle between hiding and showing the active panel */
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
